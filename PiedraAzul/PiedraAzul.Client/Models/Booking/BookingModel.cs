8b9c3b0a61ed1e56087d7c6647c03c54d53f4e8d
JhersonCastro <jhersoncastro@unicauca.edu.co>
2026-03-21 22:59:16 -0500

    Add AppointmentSchedulerModel and update BookingModel
    Introduce AppointmentSchedulerModel for slot details and update BookingModel to include required Id, Doctor (DoctorModel), and AppointmentSchedulerModel properties. Change namespace to PiedraAzul.Client.Models.Booking for better organization and structure.

--- PiedraAzul/PiedraAzul.Client/Models/Booking/BookingModel.cs
+++ PiedraAzul/PiedraAzul.Client/Models/Booking/BookingModel.cs
@@ -1,9 +1,13 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using PiedraAzul.Client.Models.UserProfiles;
+using System.ComponentModel.DataAnnotations;
 
-namespace PiedraAzul.Client.Models
+namespace PiedraAzul.Client.Models.Booking
 {
     public class BookingModel
     {
+        [Required]
+        [MinLength(5, ErrorMessage = "El ID debe tener al menos 5 caracteres")]
+        public string Id { get; set; }
         [Required(ErrorMessage = "El nombre es obligatorio")]
         [MinLength(3, ErrorMessage = "El nombre es muy corto")]
         public string Nombre { get; set; }
@@ -16,8 +20,14 @@ namespace PiedraAzul.Client.Models
         public string Direccion { get; set; }
         [Required(ErrorMessage = "El doctor es obligatorio")]
         public string DoctorId { get; set; }
+
+        public DoctorModel Doctor { get; set; }
+
         [Required(ErrorMessage = "Por favor selecciona una horario para la cita")]
         public string SlotId { get; set; }
+
+        public AppointmentSchedulerModel AppointmentSchedulerModel { get; set; }
+
         public DateTime DayOfYear { get; set; }
     }
 
