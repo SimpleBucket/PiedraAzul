89eff18aab759742c492e34084a8a9dd38db87b3
JhersonCastro <jhersoncastro@unicauca.edu.co>
2026-04-07 12:09:36 -0500

    Add Mediator command/handler for appointment creation
    Introduced CreateAppointmentCommand and CreateAppointmentHandler to implement appointment creation using the Mediator pattern. Includes validation for doctor, slot, and patient existence, slot availability checks, and exception handling for invalid cases. This improves separation of concerns and maintainability.

---
+++ PiedraAzul/PiedraAzul.Application/Features/Appointments/CreateAppointment/CreateAppointmentCommand.cs
@@ -0,0 +1,17 @@
+﻿using Mediator;
+using PiedraAzul.Domain.Entities.Operations;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PiedraAzul.Application.Features.Appointments.CreateAppointment
+{
+    public record CreateAppointmentCommand(
+    string DoctorId,
+    Guid SlotId,
+    DateOnly Date,
+
+    string? PatientUserId,
+    string? PatientGuestId
+) : IRequest<Appointment>;
+}
