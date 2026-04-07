1a3158564dbbb85cd57d92e830b5c285a3b61e3d
JhersonCastro <jhersoncastro@unicauca.edu.co>
2026-04-07 12:08:37 -0500

    Add models to application

---
+++ PiedraAzul/PiedraAzul.Application/Common/Models/Cache/SlotState.cs
@@ -0,0 +1,10 @@
+﻿namespace PiedraAzul.Application.Common.Models.Cache;
+
+public class SlotState
+{
+    public string SlotId { get; set; } = default!;
+    public bool IsReserved { get; set; }
+
+    public string? LockedBy { get; set; }
+    public DateTime? LockExpiresAt { get; set; }
+}
