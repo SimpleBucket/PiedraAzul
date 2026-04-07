965bc2813eb75ab09ca92b4b43d6977264fa51db
JhersonCastro <jhersoncastro@unicauca.edu.co>
2026-04-07 12:06:07 -0500

    Add CreateContracts utility for GuestPatientInput creation
    Introduced a static CreateContracts class in PiedraAzul.Client.Services.Utils with a method to simplify the creation of GuestPatientInput objects. Added necessary using directives for Shared.Grpc and System.Reflection.Metadata.

---
+++ PiedraAzul/PiedraAzul.Client/Services/Utils/CreateContracts.cs
@@ -0,0 +1,19 @@
+﻿using Shared.Grpc;
+using System.Reflection.Metadata;
+
+namespace PiedraAzul.Client.Services.Utils
+{
+    public static class CreateContracts
+    {
+        public static GuestPatientInput CreateGuestPatientInput(string patientName, string patientPhone, string patientIdentification, string extraInfo)
+        {
+            return new GuestPatientInput
+            {
+                Name = patientName,
+                Phone = patientPhone,
+                Identification = patientIdentification,
+                ExtraInfo = extraInfo
+            };
+        }
+    }
+}
