0a3f1a192f1dda436d732e15e944616c4a13c18b
JhersonCastro <jhersoncastro@unicauca.edu.co>
2026-04-07 11:33:51 -0500

    Add SeedExtension

---
+++ PiedraAzul/PiedraAzul/Extensions/SeedExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+using PiedraAzul.Infrastructure.Identity;
+using PiedraAzul.Infrastructure.Persistence;
+using PiedraAzul.Seeders;
+
+namespace PiedraAzul.Extensions
+{
+    public static class SeedExtensions
+    {
+        public static async Task SeedAsync(this WebApplication app)
+        {
+            using var scope = app.Services.CreateScope();
+
+            var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+            var userManager = scope.ServiceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+
+            await DbSeeder.SeedAsync(context, userManager);
+        }
+    }
+}
