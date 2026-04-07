5efee713a59a6a0008070d6586910d2bbbb8a95e
JhersonCastro <jhersoncastro@unicauca.edu.co>
2026-04-07 12:08:50 -0500

    Add interfaces that infra must be implemented

---
+++ PiedraAzul/PiedraAzul.Application/Common/Interfaces/IRefreshTokenService.cs
@@ -0,0 +1,15 @@
+﻿
+    namespace PiedraAzul.Application.Common.Interfaces
+    {
+        public interface IRefreshTokenService
+        {
+            // genera un token, lo guarda en la base de datos y lo retorna
+            Task<string> GenerateRefreshTokenAsync(string userId);
+
+            // retorna userId
+            Task<string?> ValidateRefreshTokenAsync(string token);
+            
+            // recibe token viejo, retorna nuevo
+            Task<string> RotateRefreshTokenAsync(string token);
+        }
+    }
