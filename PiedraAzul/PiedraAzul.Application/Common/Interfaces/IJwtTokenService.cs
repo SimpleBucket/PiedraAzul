6c3162197bf2f3c3d8e64081b8fe9651472e351c
JhersonCastro <jhersoncastro@unicauca.edu.co>
2026-04-07 12:08:50 -0500

    Add interfaces that infra must be implemented

---
+++ PiedraAzul/PiedraAzul.Application/Common/Interfaces/IJwtTokenService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PiedraAzul.Application.Common.Interfaces
+{
+    public interface IJwtTokenService
+    {
+        Task<string> CreateTokenAsync(string userId, List<string> roles);
+        Task<string?> GetUserIdByToken(string token);
+    }
+}
