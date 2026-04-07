11020336d140d7066efca3e6b8b08bf0e0373bf8
JhersonCastro <jhersoncastro@unicauca.edu.co>
2026-04-07 11:34:13 -0500

    Add SignalRHubsExtension

---
+++ PiedraAzul/PiedraAzul/Extensions/HubExtensions.cs
@@ -0,0 +1,10 @@
+﻿//using PiedraAzul.RealTime.Hubs;
+namespace PiedraAzul.Extensions;
+public static class HubExtensions
+{
+    public static WebApplication MapHubs(this WebApplication app)
+    {
+        //app.MapHub<AppointmentHub>("/hubs/appointments");
+        return app;
+    }
+}
