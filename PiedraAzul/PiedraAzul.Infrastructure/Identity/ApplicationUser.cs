f24ea4adbd1eb38ab22d2fd4d7b5a7d9d6082ede
JhersonCastro <jhersoncastro@unicauca.edu.co>
2026-04-06 22:28:16 -0500

    Refactor identity and add AppDbContext to Infrastructure
    Moved and enhanced ApplicationUser to Infrastructure.Identity with new properties and updated enum usage. Introduced AppDbContext in Infrastructure.Persistence, centralizing DbSets for domain entities and applying model configurations. This refactors identity and data context management into the Infrastructure layer.

--- PiedraAzul/PiedraAzul.Infrastructure/Identity/ApplicationUser.cs
+++ PiedraAzul/PiedraAzul.Infrastructure/Identity/ApplicationUser.cs
@@ -1,18 +1,15 @@
 ﻿using Microsoft.AspNetCore.Identity;
-using PiedraAzul.Data.Models;
+using PiedraAzul.Domain.Entities.Shared.Enums;
 
-namespace PiedraAzul.Data
+namespace PiedraAzul.Infrastructure.Identity
 {
     public class ApplicationUser : IdentityUser
     {
         public string IdentificationNumber { get; set; } = string.Empty;
-        public DateTime created_at { get; init; } = DateTime.UtcNow;
+        public DateTime CreatedAt { get; init; } = DateTime.UtcNow;
         public GenderType Gender { get; set; } = GenderType.NonSpecified;
         public DateTime? BirthDate { get; set; }
-        public string Name { get; set; }
+        public string Name { get; set; } = string.Empty;
         public string AvatarUrl { get; set; } = "default.png";
-
-        public virtual PatientProfile? PatientProfile{ get; set; }
-        public virtual DoctorProfile? DoctorProfile{ get; set; }
     }
 }
---
+++ PiedraAzul/PiedraAzul.Infrastructure/Persistence/AppDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using PiedraAzul.Domain.Entities.Config;
+using PiedraAzul.Domain.Entities.Operations;
+using PiedraAzul.Domain.Entities.Profiles.Doctor;
+using PiedraAzul.Domain.Entities.Profiles.Patients;
+using PiedraAzul.Infrastructure.Identity;
+
+namespace PiedraAzul.Infrastructure.Persistence
+{
+    public class AppDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public DbSet<Appointment> Appointments => Set<Appointment>();
+        public DbSet<Doctor> Doctors => Set<Doctor>();
+        public DbSet<DoctorAvailabilitySlot> Slots => Set<DoctorAvailabilitySlot>();
+        public DbSet<RegisteredPatient> Patients => Set<RegisteredPatient>();
+        public DbSet<GuestPatient> Guests => Set<GuestPatient>();
+        public DbSet<SystemConfig> SystemConfigs => Set<SystemConfig>();
+
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+        }
+    }
+}
