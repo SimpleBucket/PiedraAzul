1f0d37d1933b3949078c11a42b19f42d1a20a52d
JhersonCastro <jhersoncastro@unicauca.edu.co>
2026-04-07 11:33:39 -0500

    Refactor auth setup into separate AuthExtensions class
    Moved JWT authentication and authorization configuration from ServiceCollectionExtensions.cs to a new AuthExtensions.cs file. Introduced AddAuth extension method for cleaner separation of concerns. Simplified JWT setup and removed related usings from ServiceCollectionExtensions.cs. Lucene and mapping service registrations remain unchanged.

---
+++ PiedraAzul/PiedraAzul/Extensions/AuthExtensions.cs
@@ -0,0 +1,52 @@
+﻿using Grpc.Net.Client;
+using Lucene.Net.Analysis;
+using Lucene.Net.Analysis.Standard;
+using Lucene.Net.Index;
+using Lucene.Net.Store;
+using Lucene.Net.Util;
+using Lucene.Net.Util.Fst;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.IdentityModel.Tokens;
+using PiedraAzul.ApplicationServices.AutoCompleteServices;
+using PiedraAzul.Client.Services;
+using PiedraAzul.Client.States;
+using Shared.Grpc;
+using System.Security.Claims;
+using System.Text;
+
+namespace PiedraAzul.Extensions
+{
+    public static  class AuthExtensions
+    {
+        public static IServiceCollection AddAuth(
+                this IServiceCollection services,
+                IConfiguration config)
+        {
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    var key = Encoding.UTF8.GetBytes(config["Jwt:Key"]!);
+
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(key),
+
+                        ValidateIssuer = true,
+                        ValidIssuer = config["Jwt:Issuer"],
+
+                        ValidateAudience = true,
+                        ValidAudience = config["Jwt:Audience"],
+
+                        ValidateLifetime = true,
+                        ClockSkew = TimeSpan.Zero
+                    };
+                });
+
+            services.AddAuthorization();
+
+            return services;
+        }
+    }
+}
