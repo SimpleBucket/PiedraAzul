fd3f97334cc9cdbd6c6da58db7f4622f30304f4b
SimpleBucket <jhersonmartinez2005@gmail.com>
2026-04-28 09:36:46 -0500

    Refactor email templates, add email change fields to users
    Refactored EmailTemplates to use shared CSS style constants and updated copyright year to 2026. Added EF Core migration to support email change workflow by introducing EmailChangeCode, EmailChangeCodeExpiresAt, and PendingEmail fields to AspNetUsers. Updated model snapshot accordingly.

--- PiedraAzul/PiedraAzul.Infrastructure/Persistence/Migrations/20260427230351_AddEmailChangeFields.Designer.cs
+++ PiedraAzul/PiedraAzul.Infrastructure/Persistence/Migrations/20260427230351_AddEmailChangeFields.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using PiedraAzul.Infrastructure.Persistence;
@@ -11,9 +12,11 @@ using PiedraAzul.Infrastructure.Persistence;
 namespace PiedraAzul.Infrastructure.Persistence.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260427230351_AddEmailChangeFields")]
+    partial class AddEmailChangeFields
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
